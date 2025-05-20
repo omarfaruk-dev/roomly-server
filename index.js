7d6cce2ff9bfd1bfb4be940fa098cbe7fe87fa88
Omar Faruk <omarfaruk.dev@gmail.com>
2025-05-20 20:01:55 +0600

    get single roommate data using get method added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -35,6 +35,13 @@ async function run() {
             const result = await roommateCollection.find().toArray();
             res.send(result);
         })
+        //get a single roommate data
+        app.get('/roommates/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await roommateCollection.findOne(query);
+      res.send(result);
+    })
         // post / create a post
         app.post('/roommates', async (req, res) => {
             const postData = req.body;
