7fed1985ce7d8aeb4301ef6589a3b3c9691dce0b
Omar Faruk <omarfaruk.dev@gmail.com>
2025-05-21 20:05:45 +0600

    delete method for roommate post added

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
             res.send(result);
         });
 
+        //delete a roommate post
+     app.delete('/roommates/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roommateCollection.deleteOne(query);
+      res.send(result);
+    })
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
